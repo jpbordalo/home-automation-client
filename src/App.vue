e53b858dc388f8c329a3173c61e7ba636061e831
jpbordalo <jpbordalo@gmail.com>
2021-01-17 22:37:18 +0000

    Vue.js + Vuetify

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-main>
+        <v-card> App </v-card>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
